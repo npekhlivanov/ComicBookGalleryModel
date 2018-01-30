32328a8d5aa494918cfe3789fa5df0d47e86e539
npekhlivanov <nicko@omegasoft.bg>
2018-01-31 01:11:45 +0200

    Added data anotations

--- ComicBookGalleryModel/Context.cs
+++ ComicBookGalleryModel/Context.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,23 @@ namespace ComicBookGalleryModel
             //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>()); // default behaviour
             //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
             Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+
+            this.Configuration.LazyLoadingEnabled = false; // Turn off lazy loading for all entities
         } 
         //public Context() : base("ComicBookGallery") { } // provide database name or connection string or add connection string with the context class name in the config file
         //public Context() : base(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ComicBookGallery3;Integrated Security=True;MultipleActiveResultSets=True") { }
+
         public DbSet<ComicBook> ComicBooks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+            //modelBuilder.Conventions.Add(new DecimalPropertyConvention(5, 2));
+            modelBuilder.Entity<ComicBook>() // use this mechanism to specify individual column properties
+                .Property(cb => cb.AverageRating)
+                .HasPrecision(5, 2);
+        }
     }
 }
--- ComicBookGalleryModel/Models/Artist.cs
+++ ComicBookGalleryModel/Models/Artist.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ComicBookGalleryModel.Models
 {
+    // use [Table("Talent")] to specify different table name
     public class Artist
     {
         public Artist()
@@ -13,6 +15,7 @@ namespace ComicBookGalleryModel.Models
             ComicBooks = new List<ComicBookArtist>();
         }
         public int Id { get; set; }
+        [Required, StringLength(100)] // use Column("FullName") to specify different column name
         public string Name { get; set; }
         public ICollection<ComicBookArtist> ComicBooks { get; set; }
     }
